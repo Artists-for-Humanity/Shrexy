5ddb9424db89ca54a4834ea033f3bdca4abc5d3c
Jonathan <Jwu28@bostonk12.org>
2022-02-10 17:55:22 -0500

    positioned log model

--- src/scripts/Scenes/GameScene.js
+++ src/scripts/Scenes/GameScene.js
@@ -11,12 +11,16 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('shrek', new URL('../../assets/newshrek.png', import.meta.url).href);
+    this.load.image('shrek', new URL('../../assets/shrek.png', import.meta.url).href);
     this.load.image('shrek-2', new URL('../../assets/shrek-crouch.png', import.meta.url).href);
+    this.load.image('stick', new URL('../../assets/log.png', import.meta.url).href);
   }
-
+  //Spawns in Shrek on the X-axis
+  // Spawns in Stick on the opposite side of Shrek
   create() {
     this.player = new Player(this, this.game.config.width / 4, this.game.config.height / 2);
+    // eslint-disable-next-line prettier/prettier
+    this.obstacle = this.add.image((this.game.config.width / 2) + 450, (this.game.config.height / 2) + 380, 'stick');
   }
 
   update() {
@@ -26,9 +30,45 @@ export default class GameScene extends Phaser.Scene {
 
   //The bird sprite to approach shrek at an y-axis base with a starting speed
 
-  //Shrek loses an hitpoint as Shrek touches either obstacle
-
-  //change the Shrek.png to an blinking-Shrek.png to indicate that Shrek had lost an hitpoint
-
   //Input Knights.png in the side of the frame
+
+  //
+
+  //Increase the score count based on the amount of coins collected or whilst collected.
+
+  //
+
+  //If the Game Over screen is up; allow option for player to play again, or to access the shop
+
+  //if player is in Shop, allow option to exit or buy
+
+  //If player is buying an item; check the condition if the amount is True for item to be purchased with current amount of coins
+
+  //If player doesnt have enough, input an pop-out, mentioning that the amount of coins are insufficient
+
+  //If player does have enough, input an pop-out, mentioning that the item had been purchased
+
+  //At the start of every game, allow the player to choose an option o whether equip an item bought from the store
+
+  //
+
+  //if Game Over screen is up; allow option for player to access the settings
+
+  // Once clicked, pop out different imaegs to represent its function
+
+  //If player clicks on "General", allow the player to change the general settings
+
+  //If player clicks on "Audio", allow the player to change the audio
+
+  //If player clicks on "Control" allow the player to switch the controls or binds
+
+  //If player clicks on "Screen" allow the player to change aspects of the screen management of the game
+
+  //Allow the player to exit the settings; back to the play again screen
+
+  //
+
+  //If player is done playing, after either an "Back" or an "Game over"; give access to an "Quit" button to exit the game
+
+  // If the Quit button is clicked, shut down all service of the game
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ import GameScene from './Scenes/GameScene';
 // Set configuration for phaser game instance
 const config = {
   type: Phaser.AUTO,
-  width: 960*1.2,
-  height: 720*1.2,
+  width: 960 * 1.2,
+  height: 720 * 1.2,
 
   // Add physics, arcade, scene, and audio
   physics: {

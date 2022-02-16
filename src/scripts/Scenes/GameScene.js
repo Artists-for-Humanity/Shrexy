900b65a73eb46dc71f77d79b8ef16263a99d9f98
Jamie De Los Santos <77027304+itisjamied@users.noreply.github.com>
2022-02-16 15:32:21 -0500

    customers move towards center

--- src/scripts/Scenes/GameScene.js
+++ src/scripts/Scenes/GameScene.js
@@ -16,11 +16,19 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('player', new URL('../../assets/player.png', import.meta.url).href);
     this.load.image('line', new URL('../../assets/line.png', import.meta.url).href);
     this.load.image('start', new URL('../../assets/burger.png', import.meta.url).href);
+    this.load.image('person1', new URL('../../assets/person1.png', import.meta.url).href);
+    this.load.image('person2', new URL('../../assets/person2.png', import.meta.url).href);
+    this.load.image('person3', new URL('../../assets/person3.png', import.meta.url).href);
+    this.load.image('person4', new URL('../../assets/person4.png', import.meta.url).href);
   }
 
   create() {
     this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2);
     this.line = new Line(this, this.game.config.width / 2, this.game.config.height / 2);
+    this.person1 = new Customer(this, 200, 100, 'person1');
+    this.person2 = new Customer(this, 600, 100, 'person2');
+    this.person3 = new Customer(this, 200, 600, 'person3');
+    this.person4 = new Customer(this, 600, 600, 'person4');
     // if(difficulty = 'easy') {
     //   easyCustomrs = ['c1', 'c2']
     // }
@@ -30,5 +38,9 @@ export default class GameScene extends Phaser.Scene {
 
   update() {
     this.line.update();
+    this.person1.update();
+    this.person2.update();
+    this.person3.update();
+    this.person4.update();
   }
 }
--- src/scripts/Sprites/Customer.js
+++ src/scripts/Sprites/Customer.js
@@ -7,14 +7,42 @@ export default class Customer extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.world.enableBody(this);
     this.setCollideWorldBounds(true);
-    this.setScale(0.5);
+    this.setScale(0.15);
+
+    // this.physics.moveTo(480, 360, 100);
+    this.scene.physics.moveToObject(this, this.scene.player, 200);
 
     return this;
   }
+  //   preload() {
+  //     this.load.image('person1', new URL('../../assets/person1.png', import.meta.url).href);
+  //     // this.load.image('person2', new URL('../../assets/person2.png', import.meta.url).href);
+  //     // this.load.image('person3', new URL('../../assets/person3.png', import.meta.url).href);
+  //     // this.load.image('person4', new URL('../../assets/person4.png', import.meta.url).href);
+  //     // this.load.image('person5', new URL('../../assets/person5.png', import.meta.url).href);
+  //     // this.load.image('person6', new URL('../../assets/person6.png', import.meta.url).href);
+  //     // this.load.image('person7', new URL('../../assets/person7.png', import.meta.url).href);
+  //     // this.load.image('person8', new URL('../../assets/person8.png', import.meta.url).href);
+  //   }
+
+  //   create() {
+  //     this.person1 = new Customer(this, 0, 0, 'person1');
+  //   }
+
+  update() {
+    console.log(this.body.x, this.body.y);
+    // if (this.x > 480 && this.y < 360) {
+    //   this.setX((this.x -= Math.random(1 - 10)));
+    //   this.setY((this.y += Math.random(1 - 10)));
+    // } else if (this.x < 480 && this.y < 360) {
+    //   this.setX((this.x += Math.random(1 - 10)));
+    //   this.setY((this.y += Math.random(1 - 10)));
+    // } else if (this.x > 480 && this.y > 360) {
+    //   this.setX((this.x += Math.random(1 - 10)));
+    //   this.setY((this.y += Math.random(1 - 10)));
+    // } else if (this.x < 480 && this.y > 360) {
+    //   this.setX((this.x += Math.random(1 - 10)));
+    //   this.setY((this.y += Math.random(1 - 10)));
+    // }
+  }
 }
-
-// const customer1 = new Customer(this.scene, 0, 0, 'person1');
-
-// update() {
-//     customer1.setX( )
-// }

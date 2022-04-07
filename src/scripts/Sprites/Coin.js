dca6298b0b6ef778fd70b3cda3998470a851d4ab
kairo8735 <83092308+kairo8735@users.noreply.github.com>
2022-04-07 15:29:04 -0400

    Update Coin.js

--- src/scripts/Sprites/Coin.js
+++ src/scripts/Sprites/Coin.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 
 export default class Coin extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) { 
+    constructor(scene, x, y) {
         super(scene, x, y, 'coinanim');
         scene.add.existing(this);
         scene.physics.world.enableBody(this);
@@ -9,7 +9,8 @@ export default class Coin extends Phaser.Physics.Arcade.Sprite {
         // this.setOrigin(0.5, 1);
         this.scaleX = .75;
         this.setSize(100, 50);
-        this.type= "coin";
-    
+        this.type = "coin";
+
         return this;
-      }}
+    }
+}

08937741154a4ff51311f98f72f9f7949234d961
kairo8735 <83092308+kairo8735@users.noreply.github.com>
2022-04-13 17:53:41 -0400

    added rock button

--- src/scripts/Scenes/GameOverScene.js
+++ src/scripts/Scenes/GameOverScene.js
@@ -1,57 +1,64 @@
 import Phaser from 'phaser';
 import WebFont from 'webfontloader';
-
+import {
+  colors
+} from '../constants';
 
 export default class GameOverScene extends Phaser.Scene {
-  constructor() {
-    super({
-      key: 'GameOverScene'
-    });
-  }
-
-  preload() {
-    this.load.image('GameOver', new URL('../../assets/gameover.png',
-      import.meta.url).href);
-
-  }
-
-  create() {
-
-    this.tintedTitle = this.add.text(this.game.config.width / 4.4, 75, 'LEADERBOARD', {
-      fontFamily: 'OCR A Std',
-      fontSize: '76px',
-      fontStyle: 'bold',
-      fill: colors.white,
-      align: 'center',
-     });
-
-     
-    WebFont.load({
-      custom: {
-        families: ['Slackey'],
-      },
-      active: () => {
-        this.add.text(this.game.config.width * .5, this.game.config.height * .9, 'Press Space', {
-            fontFamily: 'Luminari Regular',
-            fontSize: '100px',
-            fill: colors.white,
-            align: 'center',
-            fontStyle: 'normal',
-            stroke: '#000000',
-            strokeThickness: 8,
-            shadow: {
-              blur: 42
-            }
-          })
-          .setOrigin(0.5, 1.7);
-
-        },
+    constructor() {
+      super({
+        key: 'GameOverScene'
       });
-      
-      this.add.image(this.game.config.width / 2, this.game.config.height * .5, 'GameOver');
+      this.gameOverText;
+    }
 
-    this.input.keyboard.on('keydown-SPACE', () => {
-      this.scene.start('MenuScene');
-    });
+    preload() {
+      this.load.image('GameOver', new URL('../../assets/gameover.png',
+        import.meta.url).href);
+
+    }
+
+    create() {
+      this.add.image(this.game.config.width / 2, this.game.config.height * .5, 'GameOver');
+      this.gameOverText = this.add.text(this.game.config.width / 2, this.game.config.height * .9, 'Press Space', {
+        fontFamily: 'Luminari Regular',
+        fontSize: '100px',
+        fill: colors.white,
+        align: 'center',
+        fontStyle: 'normal',
+        stroke: '#000000',
+        strokeThickness: 8,
+        shadow: {
+          blur: 42
+        }
+      });
+
+      // active: () => {
+
+
+
+
+        //   this.add.text(this.game.config.width * .5, this.game.config.height * .9, 'Press Space', {
+        //         fontFamily: 'Luminari Regular',
+        //         fontSize: '100px',
+        //         fill: colors.white,
+        //         align: 'center',
+        //         fontStyle: 'normal',
+        //         stroke: '#000000',
+        //         strokeThickness: 8,
+        //         shadow: {
+        //           blur: 42
+        //         }
+        //       })
+        //       .setOrigin(0.5, 1.7);
+
+        //     },
+
+
+
+        this.input.keyboard.on('keydown-SPACE', () => {
+          this.scene.start('MenuScene');
+        });
+      // }
+    }
   }
-}
--- src/scripts/Scenes/MenuScene.js
+++ src/scripts/Scenes/MenuScene.js
@@ -5,83 +5,127 @@ import {
 } from '../constants';
 
 export default class MenuScene extends Phaser.Scene {
-    constructor() {
-      super({
-        key: 'MenuScene'
-      });
-    }
-
-    preload() {
-      this.load.image('background-3', new URL('../../assets/menu.png',
-        import.meta.url).href);
-      this.load.image('logo', new URL('../../assets/shrexy_logo.png',
-        import.meta.url).href);
-      this.load.image('lilypad', new URL('../../assets/lilypad.png',
-        import.meta.url).href);
-      this.load.image('rock', new URL('../../assets/rock.png',
-        import.meta.url).href);
-
-    }
-
-    create() {
-      this.add.image(this.game.config.width / 2, this.game.config.height * .425, 'background-3').setScale(1.1, 1.1);
-      this.play = this.add.image(this.game.config.width / 3.5, this.game.config.height * .45, 'lilypad').setScale(0.9, 0.9);
-
-      WebFont.load({
-        custom: {
-          families: ['Slackey'],
-        },
-        active: () => {
-          this.add.text(this.game.config.width * .285, this.game.config.height * .63, 'Play', {
-              fontFamily: 'Luminari Regular',
-              fontSize: '100px',
-              fill: colors.white,
-              align: 'center',
-              fontStyle: 'normal',
-              stroke: '#000000',
-              strokeThickness: 8,
-              shadow: {
-                blur: 42
-              }
-            })
-            .setOrigin(0.5, 1.7);
-          this.add.text(this.game.config.width / 2, this.game.config.height * .287, 'Shrexy', {
-              fontFamily: 'Luminari Regular',
-              fontSize: '120px',
-              fill: colors.shrexyGreen,
-              align: 'center',
-              fontStyle: 'italic',
-              stroke: '#000000',
-              strokeThickness: 8,
-              shadow: {
-                blur: 42
-              }
-            })
-            .setOrigin(0.5, 1.7);
-          this.add.text(this.game.config.width * .5, this.game.config.height * .29, 'Escape Lord Farquaad!', {
-              fontFamily: 'Luminari Regular',
-              fontSize: '55px',
-              fill: colors.escapeGold,
-              align: 'center',
-              fontStyle: 'italic',
-              stroke: '#000000',
-              strokeThickness: 8,
-              shadow: {
-                blur: 42
-              }
-            })
-            .setOrigin(0.5, 1.7);
-
-        },
-      });
-
-      this.input.keyboard.on('keydown-UP', () => {
-        this.scene.start('GameScene');
-      });
-
-      this.play.setInteractive();
-      this.play.on('pointerdown', () => {
-        this.scene.start('GameScene');
-      });
-    }
+  constructor() {
+    super({
+      key: 'MenuScene'
+    });
   }
+
+  preload() {
+    this.load.image('background-3', new URL('../../assets/menu.png',
+      import.meta.url).href);
+    this.load.image('logo', new URL('../../assets/shrexy_logo.png',
+      import.meta.url).href);
+    this.load.image('lilypad', new URL('../../assets/lilypad.png',
+      import.meta.url).href);
+    this.load.image('rock', new URL('../../assets/rock.png',
+      import.meta.url).href);
+
+  }
+
+  create() {
+    this.add.image(this.game.config.width / 2, this.game.config.height * .425, 'background-3').setScale(1.1, 1.1);
+    this.play = this.add.image(this.game.config.width / 3.5, this.game.config.height * .45, 'lilypad').setScale(0.9, 0.9);
+
+    var button = this.add.image(this.game.config.width * .7, this.game.config.height * .45, 'rock').setInteractive();
+    button.on('pointerdown', () => {
+      console.log('hello');
+      var url = 'index.html';
+      var s = window.open(url, '_blank');
+      if (s && s.focus) {
+        s.focus();
+      } else if (!s) {
+        window.location.href = url;
+      }
+
+      console.log(s);
+
+
+
+    });
+
+
+    // button.on('pointerdown', this.openExternalLink(), this);
+
+    WebFont.load({
+      custom: {
+        families: ['Slackey'],
+      },
+      active: () => {
+        this.add.text(this.game.config.width * .285, this.game.config.height * .63, 'Play', {
+            fontFamily: 'Luminari Regular',
+            fontSize: '100px',
+            fill: colors.white,
+            align: 'center',
+            fontStyle: 'normal',
+            stroke: '#000000',
+            strokeThickness: 8,
+            shadow: {
+              blur: 42
+            }
+          })
+          .setOrigin(0.5, 1.7);
+        this.add.text(this.game.config.width / 2, this.game.config.height * .287, 'Shrexy', {
+            fontFamily: 'Luminari Regular',
+            fontSize: '120px',
+            fill: colors.shrexyGreen,
+            align: 'center',
+            fontStyle: 'italic',
+            stroke: '#000000',
+            strokeThickness: 8,
+            shadow: {
+              blur: 42
+            }
+          })
+          .setOrigin(0.5, 1.7);
+        this.add.text(this.game.config.width * .5, this.game.config.height * .29, 'Escape Lord Farquaad!', {
+            fontFamily: 'Luminari Regular',
+            fontSize: '55px',
+            fill: colors.escapeGold,
+            align: 'center',
+            fontStyle: 'italic',
+            stroke: '#000000',
+            strokeThickness: 8,
+            shadow: {
+              blur: 42
+            }
+          })
+          .setOrigin(0.5, 1.7);
+          this.add.text(this.game.config.width * .7, this.game.config.height * .63, 'Back to \nShrexy Page', {
+            fontFamily: 'Luminari Regular',
+            fontSize: '50px',
+            fill: colors.white,
+            align: 'center',
+            fontStyle: 'normal',
+            stroke: '#000000',
+            strokeThickness: 8,
+            shadow: {
+              blur: 42
+            }
+          })
+          .setOrigin(0.5, 1.7);
+      },
+    });
+
+    this.input.keyboard.on('keydown-UP', () => {
+      this.scene.start('GameScene');
+    });
+
+    this.play.setInteractive();
+    this.play.on('pointerdown', () => {
+      this.scene.start('GameScene');
+    });
+  }
+
+  // openExternalLink() {
+  //   // var tweet = 'I am testing a button from within a Phaser example';
+  //   var url = 'google.com';
+  //   var s = window.open(url, '_blank');
+
+  //   if (s && s.focus) {
+  //     s.focus();
+  //   } else if (!s) {
+  //     window.location.href = url;
+  //   }
+  // }
+}

6a0ba309bfd0578d116e35f2d26caea635926fa9
Ctech 21K <ctech21k@Ctech-21Ks-iMac.local>
2022-05-10 17:26:34 -0400

    Revert "Merge branch 'main' of https://github.com/Artists-for-Humanity/Shrexy"
    This reverts commit 2eb163b5822063707e1731b09f53a055b1ff1922, reversing
    changes made to df9443ad1d8f02fdbe002c752138397b5abf9ad3.

--- src/scripts/Scenes/MenuScene.js
+++ src/scripts/Scenes/MenuScene.js
@@ -16,35 +16,21 @@ export default class MenuScene extends Phaser.Scene {
       import.meta.url).href);
     this.load.image('logo', new URL('../../assets/shrexy_logo.png',
       import.meta.url).href);
-    this.load.image('lilypad', new URL('../../assets/lilypad.png',
-      import.meta.url).href);
-    this.load.image('rock', new URL('../../assets/rock.png',
-      import.meta.url).href);
+
   }
 
   create() {
     this.add.image(this.game.config.width / 2, this.game.config.height * .425, 'background-3').setScale(1.1, 1.1);
-    this.play = this.add.image(this.game.config.width / 3.5, this.game.config.height * .45, 'lilypad').setScale(0.9, 0.9);
-
-    var button = this.add.image(this.game.config.width * .68, this.game.config.height * .49, 'rock').setInteractive().setScale(1.3, 1.3);
-    button.on('pointerdown', () => {
-      var url = 'index.html';
-      var s = window.open(url, '_blank');
-      if (s && s.focus) {
-        s.focus();
-      } else if (!s) {
-        window.location.href = url;
-      }
-    });
+    // this.add.image(this.game.config.width / 2, this.game.config.height * .4, 'logo').setScale(.4, .4);
 
     WebFont.load({
       custom: {
         families: ['Slackey'],
       },
       active: () => {
-        this.add.text(this.game.config.width * .285, this.game.config.height * .63, 'Play', {
+        this.add.text(this.game.config.width * .5, this.game.config.height * .8, 'Press Up arrow to Play', {
             fontFamily: 'Luminari Regular',
-            fontSize: '100px',
+            fontSize: '60px',
             fill: colors.white,
             align: 'center',
             fontStyle: 'normal',
@@ -55,9 +41,9 @@ export default class MenuScene extends Phaser.Scene {
             }
           })
           .setOrigin(0.5, 1.7);
-        this.add.text(this.game.config.width / 2, this.game.config.height * .287, 'Shrexy', {
+        this.add.text(this.game.config.width / 2, this.game.config.height * .9, 'Shrexy', {
             fontFamily: 'Luminari Regular',
-            fontSize: '120px',
+            fontSize: '350px',
             fill: colors.shrexyGreen,
             align: 'center',
             fontStyle: 'italic',
@@ -68,9 +54,9 @@ export default class MenuScene extends Phaser.Scene {
             }
           })
           .setOrigin(0.5, 1.7);
-        this.add.text(this.game.config.width * .5, this.game.config.height * .29, 'Escape Lord Farquaad!', {
+        this.add.text(this.game.config.width * .47, this.game.config.height * .7, 'Escape Lord Farquaad!', {
             fontFamily: 'Luminari Regular',
-            fontSize: '55px',
+            fontSize: '70px',
             fill: colors.escapeGold,
             align: 'center',
             fontStyle: 'italic',
@@ -81,28 +67,11 @@ export default class MenuScene extends Phaser.Scene {
             }
           })
           .setOrigin(0.5, 1.7);
-        this.add.text(this.game.config.width * .7, this.game.config.height * .63, 'Back to \nShrexy Page', {
-            fontFamily: 'Luminari Regular',
-            fontSize: '50px',
-            fill: colors.white,
-            align: 'center',
-            fontStyle: 'normal',
-            stroke: '#000000',
-            strokeThickness: 8,
-            shadow: {
-              blur: 42
-            }
-          })
-          .setOrigin(0.5, 1.7);
+
       },
     });
 
     this.input.keyboard.on('keydown-UP', () => {
-      this.scene.start('GameScene');
-    });
-
-    this.play.setInteractive();
-    this.play.on('pointerdown', () => {
       this.scene.start('GameScene');
     });
   }
--- src/scripts/Sprites/BirdObstacle.js
+++ src/scripts/Sprites/BirdObstacle.js
@@ -1,17 +1,36 @@
+
 export default class BirdObstacle extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'birdanim');
-    scene.add.existing(this);
-    scene.physics.world.enableBody(this);
-    this.setGravityY(4000);
+    constructor(scene, x, y) { 
+      super(scene, x, y, 'birdanim');
+      scene.add.existing(this);
+      scene.physics.world.enableBody(this);
+      this.setGravityY(4000);
+      // this.setOrigin(0.5, 1);
+      // this.scaleX = .35;
+      // this.scaleY = .35;
+      this.setScale(.75, .75);
+      this.type = "bird";
+      // this.setSize(200, 250);
+      return this;
+    }
 
-    this.setScale(.75, .75);
-    this.type = "bird";
-    return this;
-  }
+  // birdUpdate() {
+  //   this.obstacles.anims.play('fly', true);
+  // }
 
-  //Update Me
-  setYPositionUp() {
-    this.y -= 4;
-  }
+
+  // setXPosition(val) {
+  //   this.x = val;
+  // }
+
+  setYPosition(val1, val2) {
+    if (this.y < val2) {
+      this.y += 3;
+      // console.log('hello 00',val1,  val2, this.y)
+
+    } else if (this.y >= val1) {
+      this.y -= 3;
+      // console.log('hello 01')
+    }
+    }
 }
